f75eda7d1dd10a53d4d820e174befc5a54e67886
vinhuang16 <vinhuang16@gmail.com>
2016-09-22 12:35:10 -0500

    Add files via upload

---
+++ AddressExtractOutput.java
@@ -0,0 +1,144 @@
+package com.vinhuang.beans;
+
+/**
+ * POJO for the output
+ *
+ */
+public class AddressExtractOutput {
+
+	private String addressLine1;
+	private String addressLine2;
+	private String addressLine3;
+	private String city;
+	private String zip;
+
+	/**
+	 * @return the addressLine1
+	 */
+	public String getAddressLine1() {
+		return this.addressLine1;
+	}
+
+	/**
+	 * @param addressLine1 the addressLine1 to set
+	 */
+	public void setAddressLine1(String addressLine1) {
+		this.addressLine1 = addressLine1;
+	}
+
+	/**
+	 * @return the addressLine2
+	 */
+	public String getAddressLine2() {
+		return this.addressLine2;
+	}
+
+	/**
+	 * @param addressLine2 the addressLine2 to set
+	 */
+	public void setAddressLine2(String addressLine2) {
+		this.addressLine2 = addressLine2;
+	}
+
+	/**
+	 * @return the addressLine3
+	 */
+	public String getAddressLine3() {
+		return this.addressLine3;
+	}
+
+	/**
+	 * @param addressLine3 the addressLine3 to set
+	 */
+	public void setAddressLine3(String addressLine3) {
+		this.addressLine3 = addressLine3;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return this.city;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the zip
+	 */
+	public String getZip() {
+		return this.zip;
+	}
+
+	/**
+	 * @param zip the zip to set
+	 */
+	public void setZip(String zip) {
+		this.zip = zip;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#hashCode()
+	 */
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((this.addressLine1 == null) ? 0 : this.addressLine1.hashCode());
+		result = prime * result + ((this.addressLine2 == null) ? 0 : this.addressLine2.hashCode());
+		result = prime * result + ((this.addressLine3 == null) ? 0 : this.addressLine3.hashCode());
+		result = prime * result + ((this.city == null) ? 0 : this.city.hashCode());
+		result = prime * result + ((this.zip == null) ? 0 : this.zip.hashCode());
+		return result;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
+	@Override
+	public boolean equals(Object obj) {
+		if(this == obj)
+			return true;
+		if(obj == null)
+			return false;
+		if(getClass() != obj.getClass())
+			return false;
+		AddressExtractOutput other = (AddressExtractOutput) obj;
+		if(this.addressLine1 == null) {
+			if(other.addressLine1 != null)
+				return false;
+		} else if(!this.addressLine1.equals(other.addressLine1))
+			return false;
+		if(this.addressLine2 == null) {
+			if(other.addressLine2 != null)
+				return false;
+		} else if(!this.addressLine2.equals(other.addressLine2))
+			return false;
+		if(this.addressLine3 == null) {
+			if(other.addressLine3 != null)
+				return false;
+		} else if(!this.addressLine3.equals(other.addressLine3))
+			return false;
+		if(this.city == null) {
+			if(other.city != null)
+				return false;
+		} else if(!this.city.equals(other.city))
+			return false;
+		if(this.zip == null) {
+			if(other.zip != null)
+				return false;
+		} else if(!this.zip.equals(other.zip))
+			return false;
+		return true;
+	}
+
+}
---
+++ AddressExtractProcessor.java
@@ -0,0 +1,66 @@
+package com.vinhuang.processors;
+
+// Import appropriate packages
+
+public class AddressExtract1099MProcessor implements ItemProcessor<Agent, AddressExtractMOutput> {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(AddressExtractProcessor.class);
+	private static final COUNTRY_USA = "USA";
+	private Date cycleDate;
+
+	/**
+	 * Before step will run prior to processing. Logging can be added here.
+	 *
+	 * @param StepExecution stepExecution
+	 * @return
+	*/
+	@Override
+	@BeforeStep
+	public void beforeStep(StepExecution stepExecution) {
+		super.beforeStep(stepExecution);
+	}
+
+	/**
+	 * This method will extract address from agent database. It retrieves agent data from SQL
+	 * I omitted complicated logic that was previously included, and replaced it with a simple
+	 * IF statement that checks the country of origin
+	 *
+	 * @param AgentKey agentKey
+	 * @return AddressExtractOutput output
+	*/
+	@Override
+	public AddressExtractOutput process(Agent agent) throws Exception {
+		// defaultString will set the value to "" if it's null
+		AddressExtract output = new AddressExtract();
+		if(cycleDate != null && COUNTRY_USA.equalsIgnoreCase(agent.getCountry()) {
+			output.setAddressLine1(defaultString(agent.getAddressLine1()));
+			output.setAddressLine2(defaultString(agent.getAddressLine2()));
+			output.setAddressLine3(defaultString(agent.getAddressLine3()));
+			output.setCity(defaultString(agent.getCity()));
+			output.setZip(defaultString(agent.getZip()));
+		} else {
+			output.setAddressLine1(defaultString(agent.getAddressLine1()));
+			output.setAddressLine2(defaultString(agent.getAddressLine2()));
+			output.setAddressLine3(defaultString(agent.getAddressLine3()));
+			output.setCity("");
+			output.setZip("");
+		}
+		return output;
+	}
+
+	/**
+	 * This Data Access Object is used to run a SELECT query against the database
+	 * by using AgentKey as its input (translated to the WHERE clause in SQL)
+	 *
+	 * @param AgentKey agentKey
+	 * @return AddressExtractOutput output
+	*/
+	public void setAgentDataDao(AgentDataDao agentDataDao) {
+		this.agentTopicDao = agentTopicDao;
+	}
+	
+	public void setCycleDate(Date cycleDate) {
+		this.cycleDate = cycleDate;
+	}
+
+}
